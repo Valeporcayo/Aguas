aec1693b3fb025cc0a6c4a82acfc17936cd3bb4b
Rafael Carrion <a01660812@tec.mx>
2026-09-14 20:54:28 -0600

    Se añadió el struct de Reporte con propiedades base

--- Aguas!/Reporte.swift
+++ Aguas!/Reporte.swift
@@ -5,3 +5,11 @@ enum EstadoReporte: String {
     case verificado = "Verificado"
     case rechazado = "Rechazado"
 }
+
+struct Reporte {
+    let titulo: String
+    let tipoFraude: String
+    let numero: String
+    let fecha: String
+    let estado: EstadoReporte
+}
